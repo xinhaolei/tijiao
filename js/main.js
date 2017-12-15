920d85319d7b7eeaac88576b63a066662fad9d81
xinhaolei <2625844705@qq.com>
2017-12-15 09:59:11 +0800

    diyici

---
+++ js/main.js
@@ -0,0 +1,99 @@
+/* Initialize Swiper */
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    slidesPerView: 1,
+    spaceBetween: 30,
+    mousewheel: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+    },
+    loop:true,
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
+
+});
+
+/*音乐*/
+var music=document.getElementById("music");
+var music_tu=document.querySelectorAll(".music_tu")[0];
+var audio=document.querySelector("audio");
+var flag=1;
+music.onclick=function(){
+    if(flag==1){
+        music_tu.style.animation="none";
+        audio.pause();
+        flag=0;
+    }else {
+        music_tu.style.animation=" run 2s 0s linear infinite";
+        audio.play();
+        flag=1;
+    }
+};
+
+
+//创建和初始化地图函数：
+function initMap(){
+    createMap();//创建地图
+    setMapEvent();//设置地图事件
+    addMapControl();//向地图添加控件
+    addMapOverlay();//向地图添加覆盖物
+}
+function createMap(){
+    map = new BMap.Map("map");
+    map.centerAndZoom(new BMap.Point(116.577384,38.081206),16);
+}
+function setMapEvent(){
+    map.enableScrollWheelZoom();
+    map.enableKeyboard();
+    map.enableDragging();
+    map.enableDoubleClickZoom()
+}
+function addClickHandler(target,window){
+    target.addEventListener("click",function(){
+        target.openInfoWindow(window);
+    });
+}
+function addMapOverlay(){
+    var markers = [
+        {content:"我的备注",title:"泊头职业学院",imageOffset: {width:0,height:3},position:{lat:38.081774,lng:116.577384}}
+    ];
+    for(var index = 0; index < markers.length; index++ ){
+        var point = new BMap.Point(markers[index].position.lng,markers[index].position.lat);
+        var marker = new BMap.Marker(point,{icon:new BMap.Icon("http://api.map.baidu.com/lbsapi/createmap/images/icon.png",new BMap.Size(20,25),{
+            imageOffset: new BMap.Size(markers[index].imageOffset.width,markers[index].imageOffset.height)
+        })});
+        var label = new BMap.Label(markers[index].title,{offset: new BMap.Size(25,5)});
+        var opts = {
+            width: 200,
+            title: markers[index].title,
+            enableMessage: false
+        };
+        var infoWindow = new BMap.InfoWindow(markers[index].content,opts);
+        marker.setLabel(label);
+        addClickHandler(marker,infoWindow);
+        map.addOverlay(marker);
+    };
+}
+//向地图添加控件
+function addMapControl(){
+    var scaleControl = new BMap.ScaleControl({anchor:BMAP_ANCHOR_BOTTOM_LEFT});
+    scaleControl.setUnit(BMAP_UNIT_IMPERIAL);
+    map.addControl(scaleControl);
+    var navControl = new BMap.NavigationControl({anchor:BMAP_ANCHOR_TOP_LEFT,type:BMAP_NAVIGATION_CONTROL_LARGE});
+    map.addControl(navControl);
+    var overviewControl = new BMap.OverviewMapControl({anchor:BMAP_ANCHOR_BOTTOM_RIGHT,isOpen:true});
+    map.addControl(overviewControl);
+}
+var map;
+initMap();
